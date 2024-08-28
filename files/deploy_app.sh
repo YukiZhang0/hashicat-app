929108d3b52537b883bcee074beea513598f0395
YukiZhang0 <zhangxue416312874@gmail.com>
2024-08-28 06:44:44 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Now have fun with me to learn this terraform hands-on session.
   <!-- END -->
 
   </div>
